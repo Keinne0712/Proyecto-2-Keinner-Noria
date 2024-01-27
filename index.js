701a3a115c13e80adfd1a6c6eb505b20294f85b2
Keinner Noria <3b20keinnern@gmail.com>
2024-01-26 22:29:47 -0400

    numeros pares e impares del 1 al 20

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 console.log("Hola Mundo")
 
-for (let i = 1; i <= 20; i++) {
-    if (i % 2 === 0) {
-      console.log(i + " es par");
-    } else {
-      console.log(i + " es impar");
+function imprimirNumeros() {
+    for (let i = 1; i <= 20; i++) {
+      if (i % 2 === 0) {
+        console.log(i + " es par");
+      } else {
+        console.log(i + " es impar");
+      }
     }
   }
